a6b6920261c2fe603c940ea4855ad65e31c2b2d7
Troy Weaver <troylw@gmail.com>
2022-04-26 14:18:39 -0400

    Added Price Index and Create Pages

--- Server/Data/Migrations/20220426174007_FixedTeeTimeEnum.Designer.cs
+++ Server/Data/Migrations/20220426174007_FixedTeeTimeEnum.Designer.cs
@@ -4,14 +4,16 @@ using GolfCourseMVC.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GolfCourseMVC.Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220426174007_FixedTeeTimeEnum")]
+    partial class FixedTeeTimeEnum
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
